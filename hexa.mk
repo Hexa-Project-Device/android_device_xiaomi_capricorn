a23789778f9c071ac679dc8b5e059ec4776cb34b
nazunamoe <jpg3927@gmail.com>
2017-05-23 23:01:44 +0900

    capricorn: fix ota update

--- hexa.mk
+++ hexa.mk
@@ -15,4 +15,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys" \
     PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
 
+# OTA Updater for Capricorn
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.ota.romname=Hexa-Project-Capricorn \
+  ro.ota.version=$(shell date -u +%Y%m%d) \
+  ro.ota.manifest=https://raw.githubusercontent.com/Hexa-Project/release_capri/master/ota.xml
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
